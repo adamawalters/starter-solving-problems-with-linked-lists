134feaf7ef3c60741eb2f2dbc4716949fdde7ccb
adamawalters <adamawalters@gmail.com>
2023-12-07 15:06:38 -0500

    completion

--- src/reverse/reverse.js
+++ src/reverse/reverse.js
@@ -9,7 +9,19 @@ const LinkedList = require("../lib/linkedList");
  * @returns {LinkedList}
  * A linked list in reverse order
  */
-function reverseIterative(list) {}
+function reverseIterative(list) {
+
+    let node = list.head;
+    let reversed = new LinkedList();
+
+    while(node) {
+        reversed.insertAtHead(node.value);
+        node = node.next;
+    }
+
+    return reversed;
+    
+}
 
 /**
  * Recursively reverse a linked list
@@ -20,6 +32,16 @@ function reverseIterative(list) {}
  * @returns {LinkedList}
  * A linked list in reverse order
  */
-function reverseRecursive(list) {}
+function reverseRecursive(list) {
+    
+   if(list.length <=1) return list;
+
+   const head = list.head.value;
+   list.remove((node, index) => index === 0);
+   const reversedList = reverseRecursive(list);
+   reversedList.insert(head);
+   return reversedList;
+
+}
 
 module.exports = { reverseIterative, reverseRecursive };
--- src/thirdfromend/thirdfromend.js
+++ src/thirdfromend/thirdfromend.js
@@ -9,6 +9,43 @@ const LinkedList = require("../lib/linkedList");
  * null if list length < 3
  *
  */
-function thirdFromEnd(list) {}
+function thirdFromEndOld(list) {
+    if(list.length < 3) return null;
+
+    let index = 0;
+    let node = list.head;
+
+    while(node) {
+        if(index === list.length - 3) return node;
+        node = node.next;
+        index++;
+    }
+
+}
+
+function thirdFromEnd(list){
+
+    if(!list.head) return null;
+
+    let pointer1 = list.head;
+    let i = 0;
+
+    while(i<2 && pointer1.next) {
+        pointer1 = pointer1.next;
+        i++;
+    }
+
+    if(i<2) return null;
+
+    let pointer2 = list.head;
+    
+    while(pointer1.next) {
+        pointer1 = pointer1.next;
+        pointer2 = pointer2.next;
+    }
+
+    return pointer2;
+
+}
 
 module.exports = thirdFromEnd;
